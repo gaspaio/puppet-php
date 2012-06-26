f60a3ea403a990e65731b4437f64b3f05355bcf3
Rodolfo Ripado <ggaspaio@gmail.com>
2012-06-27 00:41:53 +0200

    refactored the php-cli installation classes and templates.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,57 @@
-class php($cli_ini_content = undef, $cli_ini_source = undef) {
-    include php::params, php::install, php::config
+class php(
+  $cli_display_errors         = $php::params::cli_display_errors,
+  $cli_display_startup_errors = $php::params::cli_display_startup_errors,
+  $cli_error_reporting        = $php::params::cli_error_reporting,
+  $cli_post_max_size          = $php::params::cli_post_max_size,
+  $cli_upload_max_filesize    = $php::params::cli_upload_max_filesize,
+  $purge_dirs                 = $php::params::purge_dirs
+) inherits php::params {
+
+  package { 'php-cli': 
+    name   => $php::params::cli_package_name,
+    ensure => present,
+  }
+
+  file { $php::params::extra_dir:
+    owner   => root,
+    group   => root,
+    purge   => $php::params::purge_dirs,
+    recurse => true,
+    force   => true,
+    require => Package['php-cli'],
+    ensure  => directory,
+  }
+
+  file { $php::params::conf_dir:
+    owner   => root,
+    group   => root,
+    purge   => $php::params::purge_dirs,
+    recurse => true,
+    force   => true,
+    require => Package['php-cli'],
+    ensure  => directory,
+  }
+
+  file { $php::params::cli_ini:
+    owner   => root,
+    group   => root,
+    require => Package['php-cli'],
+    ensure  => file,
+    content => template($php::params::tpl_php53cli_ini), 
+  }
+
+  file { $php::params::cli_dir:
+    owner   => root,
+    group   => root,
+    purge   => $php::params::purge_dirs,
+    recurse => true,
+    force   => true,
+    require => Package['php-cli'],
+    ensure  => directory,
+  }
+
+  file { "${php::params::cli_dir}conf.d":
+    ensure => "../conf.d",
+    require => Package['php-cli'],
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,22 +1,46 @@
 class php::params {
-    case $operatingsystem {
-        /(Ubuntu|Debian)/: {
-            $base_dir = "/etc/php5/"
-            $cli_dir = "${base_dir}cli/"
-            $cli_ini = "${cli_dir}php.ini"
-            $cli_package_name = "php5-cli"
-            $extra_dir = "${base_dir}extra/"
-            $conf_dir = "${base_dir}conf.d/"
-            $fpm_dir = "${base_dir}fpm/"
-            $fpm_pool_dir = "${fpm_dir}pool.d/"
-            $fpm_ini = "${fpm_dir}php.ini"
-            $fpm_package_name = "php5-fpm"
-            $fpm_conf = "${fpm_dir}php-fpm.conf"
-            $fpm_service_name = "php5-fpm"
-            $apache_dir = "${base_dir}apache2/"
-            $apache_ini = "${apache_dir}php.ini"
-            $apache_package_name = "libapache2-mod-php5"
-            $apache_service_name = "apache2"
-        }
+
+  # Purge directories managed by puppet ?
+  $purge_dirs = false
+
+  # php.ini default settings
+  $cli_display_errors         = 'On'
+  $cli_display_startup_errors = 'On'
+  $cli_error_reporting        = 'E_ALL | E_STRICT'
+  $cli_post_max_size          = '64M'
+  $cli_upload_max_filesize    = '64M'  
+
+  $apache_max_execution_time     = 30
+  $apache_max_input_time         = 60
+  $apache_display_errors         = 'On'
+  $apache_display_startup_errors = 'On'
+  $apache_error_reporting        = 'E_ALL | E_STRICT'
+  $apache_post_max_size          = '64M'
+  $apache_upload_max_filesize    = '64M'  
+  
+  case $operatingsystem {
+    /(Ubuntu|Debian)/: {
+      $base_dir = "/etc/php5/"
+      $extra_dir = "${base_dir}extra/"
+      $conf_dir = "${base_dir}conf.d/"
+
+      $cli_dir = "${base_dir}cli/"
+      $cli_ini = "${cli_dir}php.ini"
+      $cli_package_name = "php5-cli"
+      $tpl_php53cli_ini = "php/php-cli.ini.erb"
+
+      $fpm_dir = "${base_dir}fpm/"
+      $fpm_pool_dir = "${fpm_dir}pool.d/"
+      $fpm_ini = "${fpm_dir}php.ini"
+      $fpm_package_name = "php5-fpm"
+      $fpm_conf = "${fpm_dir}php-fpm.conf"
+      $fpm_service_name = "php5-fpm"
+
+      $apache_dir = "${base_dir}apache2/"
+      $apache_ini = "${apache_dir}php.ini"
+      $apache_package_name = "libapache2-mod-php5"
+      $apache_service_name = "apache2"
+      $tpl_php53apache_ini = "php/php-apache.ini.erb"
     }
+  }
 }
